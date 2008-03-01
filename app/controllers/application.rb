dd166e8f06dc531bba9c2264e1c738ae4532976c
Michael Diolosa <mbrio@Denali.home>
2008-02-29 22:16:23 -0500

    The clean rails environment.

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '78937a502a39198e820de78ae06c8d83'
+end
